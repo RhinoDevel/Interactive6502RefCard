8d8a421c12064e93b08e64647c4199103ed4eb8e
Marc <RhinoDevel@users.noreply.github.com>
2020-12-23 10:43:30 +0100

    Cosmetic.

--- main.js
+++ main.js
@@ -19,20 +19,21 @@
     {
         f.initBody();
 
-        rhino6502doc.initRefCard({
-            data: {
-                addrModes: rhino6502doc.addrModes,
-                mnemonics: rhino6502doc.mnemonics,
-                commands: rhino6502doc.commands,
-                flagAffect: rhino6502doc.flagAffect
-            },
-            ele: rhino6502doc.refCardUiInitAndGetEles(document.body, null),
-            helpers: {
-                obj: rhino6502doc.obj,
-                ele: rhino6502doc.ele,
-                num: rhino6502doc.num
-            }
-        });
+        rhino6502doc.initRefCard(
+			{
+				data: {
+					addrModes: rhino6502doc.addrModes,
+					mnemonics: rhino6502doc.mnemonics,
+					commands: rhino6502doc.commands,
+					flagAffect: rhino6502doc.flagAffect
+				},
+				ele: rhino6502doc.refCardUiInitAndGetEles(document.body, null),
+				helpers: {
+					obj: rhino6502doc.obj,
+					ele: rhino6502doc.ele,
+					num: rhino6502doc.num
+				}
+			});
     };
 
     window.addEventListener('load', f.onLoad);
